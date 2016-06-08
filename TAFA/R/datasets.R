6676ec7fb8c9a432be84371621fdd72eba51ffa1
zpsimpso <zpsimpso@gmail.com>
2016-06-08 11:16:10 -0500

    Ibid.

---
+++ TAFA/R/datasets.R
@@ -0,0 +1,15 @@
+#'Total phosphorus concentrations at Illinois River, 2001-2009
+#'
+#'A dataset containing TP (mg/L), flow (cfs), and sample dates from 2001 to
+#'2009. The site is Illinois River south of Siloam Springs, AR; USGS site
+#'07195430 (see waterdata.usgs.gov)
+#'
+#'@format A data frame with three columns and 73 rows.
+#'\describe{
+#'  \item{Date}{The sample dates, should be POSIXct format}
+#'  \item{Flow_cfs}{Streamflow, in cubic feet per second, numeric}
+#'  \item{TP_mgl}{Total phosphorus concentrations, mg per L, numeric}
+#'  ...
+#'}
+#'@source \url{waterdata.usgs.gov}
+"IR59"
---
+++ TAFA/R/testfxn.R
@@ -0,0 +1,17 @@
+#' A test function
+#' 
+#' This function gives insight to Zach's mental state.
+#' @param zach_angry Default is- he mad
+#' @keywords FUCK
+#' @export
+#' @examples 
+#' test()
+
+test <- function(zach_angry=TRUE){
+  if(zach_angry==TRUE){
+    print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+  }
+  else{
+    print("BULLSHIT, HE'S TOTALLY ANGRY. LOOK AT HIM.")
+  }
+}
